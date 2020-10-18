d639ef215861a080623b66ef7c802fdb919b7af1
Oleksii <o.rohochyi@astoundcommerce.com>
2020-10-19 00:15:43 +0300

    Updates

---
+++ precache-manifest.d9d41891bc5bfa67305a24fccbe73fc0.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8dd104d326c37fbbd6d6ec52283d6a48",
+    "url": "/ultra/index.html"
+  },
+  {
+    "revision": "f34fc01059707539ba99",
+    "url": "/ultra/static/js/2.b9943b6c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/ultra/static/js/2.b9943b6c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7272241e33d013d849a6",
+    "url": "/ultra/static/js/main.07f11220.chunk.js"
+  },
+  {
+    "revision": "44060db4482fbeb2645a",
+    "url": "/ultra/static/js/runtime-main.cf9dcf6f.js"
+  },
+  {
+    "revision": "87ff362e963dc52c97929e148c87ed09",
+    "url": "/ultra/static/media/img1.87ff362e.svg"
+  },
+  {
+    "revision": "8cf5a18273a9a4bbe7668280ae401a50",
+    "url": "/ultra/static/media/img2.8cf5a182.svg"
+  },
+  {
+    "revision": "a1eb41ee63a4437c425a2ace8f8716d1",
+    "url": "/ultra/static/media/img3.a1eb41ee.svg"
+  },
+  {
+    "revision": "87ff362e963dc52c97929e148c87ed09",
+    "url": "/ultra/static/media/img4.87ff362e.svg"
+  }
+]);
